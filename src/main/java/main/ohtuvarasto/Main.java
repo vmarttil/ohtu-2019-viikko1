0013c6eb9bd6fd73bb3624459b8245b13245def8
Ville Marttila <ville.marttila@iki.fi>
2019-11-08 11:14:20 +0200

    korjattu checkstyle-virhe, tehty tehtävä 3

--- src/main/java/main/ohtuvarasto/Main.java
+++ src/main/java/main/ohtuvarasto/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-            Varasto mehua = new Varasto(100.0);
-            Varasto olutta = new Varasto(100.0, 20.2);
+        Varasto mehua = new Varasto(100.0);
+        Varasto olutta = new Varasto(100.0, 20.2);
 
         System.out.println("Luonnin jälkeen:");
         System.out.println("Mehuvarasto: " + mehua);
